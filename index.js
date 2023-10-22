bff83c173dbb94c757ff0b270d4bd2e115e29c5e
Random_coder <michele.vennarini@gmail.com>
2023-10-22 02:34:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var icon = document.getElementById("icon");
+var icon1 = document.getElementById("a");
+var icon2 = document.getElementById("b");
+var icon3 = document.getElementById("c");
+var nav = document.getElementById('nav');
+var blue = document.getElementById("blue");
+
+icon.addEventListener('click', function () {
+    console.log("click")
+    icon1.classList.toggle('a');
+    icon2.classList.toggle('c');
+    icon3.classList.toggle('b');
+    nav.classList.toggle('show');
+    blue.classList.toggle('slide');
+});
+
+
